f66b642f42e7962ee22ceb3b4791b90505c76340
Heanbian <mahingtian@gmail.com>
2023-09-13 10:44:55 +0800

    1
    1

--- src/main/java/com/heanbian/block/crypto/SM4Template.java
+++ src/main/java/com/heanbian/block/crypto/SM4Template.java
@@ -4,12 +4,10 @@ import java.nio.charset.StandardCharsets;
 import java.security.Security;
 import java.util.Base64;
 
-import org.bouncycastle.crypto.BufferedBlockCipher;
-import org.bouncycastle.crypto.engines.SM4Engine;
-import org.bouncycastle.crypto.modes.CBCBlockCipher;
-import org.bouncycastle.crypto.paddings.PKCS7Padding;
-import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
-import org.bouncycastle.crypto.params.KeyParameter;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class SM4Template {
@@ -26,19 +24,11 @@ public class SM4Template {
 
 	public String encrypt(String plainText, String key) {
 		try {
-			byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
-			byte[] plaintextBytes = plainText.getBytes(StandardCharsets.UTF_8);
-
-			SM4Engine engine = new SM4Engine();
-			CBCBlockCipher cipher = (CBCBlockCipher) CBCBlockCipher.newInstance(engine);
-			BufferedBlockCipher pCipher = new PaddedBufferedBlockCipher(cipher, new PKCS7Padding());
-			pCipher.init(true, new KeyParameter(keyBytes));
-
-			byte[] encryptedBytes = new byte[pCipher.getOutputSize(plaintextBytes.length)];
-			int outputLen = pCipher.processBytes(plaintextBytes, 0, plaintextBytes.length, encryptedBytes, 0);
-			pCipher.doFinal(encryptedBytes, outputLen);
-
-			return Base64.getEncoder().encodeToString(encryptedBytes);
+			Cipher cipher = Cipher.getInstance("SM4/CBC/PKCS7Padding");
+			SecretKeySpec sm4Key = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "SM4");
+			cipher.init(Cipher.ENCRYPT_MODE, sm4Key, new IvParameterSpec(new byte[16]));
+			byte[] cipherText = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
+			return Base64.getEncoder().encodeToString(cipherText);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -50,19 +40,12 @@ public class SM4Template {
 
 	public String decrypt(String encryptedText, String key) {
 		try {
-			byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
-			byte[] encryptedBytes = Base64.getDecoder().decode(encryptedText);
-
-			SM4Engine engine = new SM4Engine();
-			CBCBlockCipher cipher = (CBCBlockCipher) CBCBlockCipher.newInstance(engine);
-			BufferedBlockCipher pCipher = new PaddedBufferedBlockCipher(cipher, new PKCS7Padding());
-			pCipher.init(false, new KeyParameter(keyBytes));
-
-			byte[] decryptedBytes = new byte[pCipher.getOutputSize(encryptedBytes.length)];
-			int outputLen = pCipher.processBytes(encryptedBytes, 0, encryptedBytes.length, decryptedBytes, 0);
-			pCipher.doFinal(decryptedBytes, outputLen);
-
-			return new String(decryptedBytes, StandardCharsets.UTF_8);
+			Cipher cipher = Cipher.getInstance("SM4/CBC/PKCS7Padding");
+			SecretKeySpec sm4Key = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "SM4");
+			cipher.init(Cipher.DECRYPT_MODE, sm4Key, new IvParameterSpec(new byte[16]));
+			byte[] decodedCipherText = Base64.getDecoder().decode(encryptedText);
+			byte[] decryptedText = cipher.doFinal(decodedCipherText);
+			return new String(decryptedText, StandardCharsets.UTF_8);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
